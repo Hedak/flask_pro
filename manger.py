68ec8d05fb4108f39f0e0b3fb3afaea0a507e4f1
li <945937725@qq.com>
2019-06-21 11:30:51 +0800

    将项目配置抽取到其它文件，启动文件之保留启动程序的相关内容

--- manger.py
+++ manger.py
@@ -1,43 +1,8 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-import redis
-from flask_wtf import CSRFProtect
-from flask_session import Session
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
+from info import app, db
 
-
-class Config(object):
-    """工程配置信息"""
-    DEBUG = True
-    SECRET_KEY = "EjpNVSNQTyGi1VvWECj9TvC/+kq3oujee2kTfQUs8yCM6xX9Yjq52v54g+HVoknA"
-    # 数据库的配置信息
-    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/information27"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # redis配置
-    REDIS_HOST = "127.0.0.1"
-    REDIS_PORT = 6379
-    # flask_session的配置信息
-    SESSION_TYPE = "redis"
-    SESSION_USE_SIGNER = True
-    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
-    # 设置需要过期
-    SESSION_PERMANENT = False
-    # 设置过期时间
-    PERMANENT_SESSION_LIFETIME = 86400 * 2
-
-
-app = Flask(__name__)
-# 加载配置
-app.config.from_object(Config)
-# 初始化数据库
-db = SQLAlchemy(app)
-redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
-# 开启当前项目csrf保护
-CSRFProtect(app)
-# 设置session保护指定位置
-Session(app)
-
+# 设置项目运行参数
 manger = Manager(app)
 # 将app与db关联
 Migrate(app, db)
