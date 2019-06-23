a04c64085a83a87e9ba11929f6ec5c520421c594
li <945937725@qq.com>
2019-06-23 10:37:14 +0800

    注册后端实现

--- info/modules/passport/views.py
+++ info/modules/passport/views.py
@@ -1,11 +1,13 @@
+from datetime import datetime
 import random
 import re
 from wsgiref import headers
 
-from flask import request, abort, current_app, make_response, jsonify
+from flask import request, abort, current_app, make_response, jsonify, session
 
-from info import redis_store, constants
+from info import redis_store, constants, db
 from info.libs.yuntongxun.sms import CCP
+from info.models import User
 from info.utils.captcha.captcha import captcha
 from info.utils.captcha.response_code import RET
 
@@ -36,7 +38,7 @@ def get_image_code():
     return response
 
 
-@passport_blu.route('/sms_code',methods=["POST"])
+@passport_blu.route('/sms_code', methods=["POST"])
 def send_sms_code():
     """生成手机验证码并返回"""
 
@@ -66,7 +68,7 @@ def send_sms_code():
         return jsonify(errno=RET.DATAERR, errmsg="验证码输入错误")
     # 如果校验通过，设置短信验证码，共六位，位数不够在前面补上零
     sms_code_str = "%06d" % random.randint(0, 999999)
-    current_app.logger.debug("短线验证码内容是: %s" % sms_code_str)
+    current_app.logger.debug("短信验证码内容是: %s" % sms_code_str)
     # 发送短信验证码
     result = CCP().send_template_sms(mobile, [sms_code_str, constants.SMS_CODE_REDIS_EXPIRES / 5], "1")
     if result != 0:
@@ -79,3 +81,48 @@ def send_sms_code():
         return jsonify(errno=RET.DBERR, errmsg="保存数据失败")
     # 告知发送结果
     return jsonify(errno=RET.OK, errmsg="发送成功")
+
+
+# 注册
+@passport_blu.route("/register", methods=["POST"])
+def register():
+    params_dict = request.json
+
+    mobile = params_dict.get("mobile")
+    password = params_dict.get("password")
+    smscode = params_dict.get("smscode")
+
+    if not all([mobile, smscode, password]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数不足")
+    if not re.match("1[35678]\\d{9}", mobile):
+        return jsonify(errno=RET.PARAMERR, errmsg="手机格式不正确")
+    # TODO:处理密码
+    try:
+        real_sms_code = redis_store.get("SMS_" + mobile)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.PARAMERR, errmsg="数据查询错误")
+    if not real_sms_code:
+        return jsonify(errno=RET.PARAMERR, errmsg="验证码已过期")
+    if real_sms_code != smscode:
+        return jsonify(errno=RET.DATAERR, errmsg="验证码输入错误")
+
+    user = User()
+    # 初始化模型
+    user.mobile = mobile
+    user.nick_name = mobile
+    user.last_login = datetime.now()
+
+    try:
+        db.session.add(user)
+        db.session.commit()
+    except Exception as e:
+        current_app.logger.error(e)
+        db.session.rollback()
+        return jsonify(errno=RET.DBERR, errmsg="数据保存失败")
+
+    session["user_id"] = user.id
+    session["mobile"] = user.mobile
+    session["nick_name"] = user.nick_name
+
+    return jsonify(errno=RET.OK, errmsg="注册成功")
