dd63ff160be4b3bbc6beee092ccfbda10b9b49c7
li <945937725@qq.com>
2019-06-25 14:02:45 +0800

    显示首页新闻列表

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,7 +1,45 @@
-from flask import render_template, current_app, session
+from flask import render_template, current_app, session, request, jsonify
 from info import redis_store
-from info.models import User, News
+from info.models import User, News, Category
+from info.utils.captcha.response_code import RET
 from . import inex_blu
+
+
+@inex_blu.route("/news_list")
+def news_list():
+    """获取首页新闻数据"""
+    cid = request.args.get("cid", "1")
+    page = request.args.get("page", "1")
+    per_page = request.args.get("per_page", "10")
+    try:
+        page = int(page)
+        cid = int(cid)
+        per_page = int(per_page)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+    filters = []
+    if cid != 1:
+        filters.append(News.category_id == cid)
+    try:
+        paginate = News.query.filter(*filters).order_by(News.create_time.desc()).paginate(page, per_page, False)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="数据查询错误")
+    news_model_list = paginate.items
+    total_page = paginate.pages
+    current_page = paginate.page
+
+    news_dict_list = []
+    for news in news_model_list:
+        news_dict_list.append(news.to_basic_dict())
+
+    data = {
+        "total_page": total_page,
+        "current_page": current_page,
+        "news_dict_list": news_dict_list
+    }
+    return jsonify(errno=RET.OK, errmsg="ok", data=data)
 
 
 # 首页
@@ -25,9 +63,16 @@ def index():
     # 遍历对象列表，将对象的字典添加到字典列表中
     for news in news_list:
         news_dict_list.append(news.to_basic_dict())
+
+    categories=Category.query.all()
+
+    category_list=[]
+    for category in  categories:
+        category_list.append(category.to_dict())
     data = {
         "user": user.to_dict() if user else None,
-        "news_dict_list": news_dict_list
+        "news_dict_list": news_dict_list,
+        "category_list":category_list
     }
 
     return render_template("news/index.html", data=data)
--- info/static/news/js/index.js
+++ info/static/news/js/index.js
@@ -1,4 +1,4 @@
-var currentCid = 0; // 当前分类 id
+var currentCid = 1; // 当前分类 id
 var cur_page = 1; // 当前页
 var total_page = 1;  // 总页数
 var data_querying = true;   // 是否正在向后台获取数据
@@ -6,6 +6,7 @@ var data_querying = true;   // 是否正在向后台获取数据
 
 $(function () {
     // 首页分类切换
+    updateNewsData()
     $('.menu li').click(function () {
         var clickCid = $(this).attr('data-cid')
         $('.menu li').each(function () {
@@ -40,11 +41,50 @@ $(function () {
         var nowScroll = $(document).scrollTop();
 
         if ((canScrollHeight - nowScroll) < 100) {
+
             // TODO 判断页数，去更新新闻数据
+            if (!data_querying){
+                data_querying=true
+                if(cur_page<total_page){
+                    cur_page+=1
+                    updateNewsData()
+                }
+            }
         }
     })
 })
 
 function updateNewsData() {
     // TODO 更新新闻数据
+    var params = {
+        "cid": currentCid,
+        "page": cur_page
+    }
+    $.get("/news_list", params, function (resp) {
+        data_querying = false
+        if (resp.errno == "0") {
+            total_page = resp.data.total_page
+
+            $(".list_con").html("")
+
+
+            for (var i = 0; i < resp.data.news_dict_list.length; i++) {
+                var news = resp.data.news_dict_list[i]
+                var content = '<li>'
+                content += '<a href="#" class="news_pic fl"><img src="' + news.index_image_url + '?imageView2/1/w/170/h/170"></a>'
+                content += '<a href="#" class="news_title fl">' + news.title + '</a>'
+                content += '<a href="#" class="news_detail fl">' + news.digest + '</a>'
+                content += '<div class="author_info fl">'
+                content += '<div class="source fl">来源：' + news.source + '</div>'
+                content += '<div class="time fl">' + news.create_time + '</div>'
+                content += '</div>'
+                content += '</li>'
+                $(".list_con").append(content)
+            }
+        }
+        else {
+            alert(resp.errmsg)
+        }
+
+    })
 }
