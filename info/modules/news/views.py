b927b5ecf1c15bec00a87d2825fdb7e6d58f2853
li <945937725@qq.com>
2019-06-25 19:33:54 +0800

    新闻详情页数据展示

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,7 +1,7 @@
 from info import constants
 from info.models import User, News
 from info.modules.news import news_blu
-from flask import render_template, session, current_app, g
+from flask import render_template, session, current_app, g, abort
 
 from info.utils.common import user_login_data
 
@@ -36,9 +36,26 @@ def news_detail(news_id):
     for news in news_list:
         news_dict_list.append(news.to_basic_dict())
 
+    # 查询新闻数据
+
+    news = None
+
+    try:
+        news = News.query.get(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+
+    if not news:
+        # 报404错误，404错误统一显示页面后续再处理
+        abort(404)
+
+    # 更新新闻的点击次数
+    news.clicks += 1
+
     data = {
         "user": user.to_dict() if user else None,
-        "news_dict_list": news_dict_list
+        "news_dict_list": news_dict_list,
+        "news": news.to_dict()
 
     }
     return render_template("news/detail.html", data=data)
