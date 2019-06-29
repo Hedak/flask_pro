5ffb406e010a214c5aa01426543ef6736ffb3ed0
li <945937725@qq.com>
2019-06-29 20:45:32 +0800

    待审核的新闻列表数据展示

--- info/modules/admin/views.py
+++ info/modules/admin/views.py
@@ -9,7 +9,7 @@ from flask import session
 from flask import url_for
 
 from info import constants
-from info.models import User
+from info.models import User, News
 from info.modules.admin import admin_blu
 from info.utils.common import user_login_data
 
@@ -159,3 +159,40 @@ def user_list():
     }
 
     return render_template('admin/user_list.html', data=data)
+
+
+@admin_blu.route("/news_review")
+def news_review():
+    page = request.args.get("p", 1)
+    try:
+        page = int(page)
+    except Exception as e:
+        current_app.logger.error(e)
+        page = 1
+
+    news_list = []
+    current_page = 1
+    total_page = 1
+
+    try:
+        paginate = News.query.filter(News.status != 0) \
+            .order_by(News.create_time.desc()) \
+            .paginate(page, constants.ADMIN_NEWS_PAGE_MAX_COUNT, False)
+
+        news_list = paginate.items
+        current_page = paginate.page
+        total_page = paginate.pages
+    except Exception as e:
+        current_app.logger.error(e)
+
+    new_dict_list = []
+    for news in news_list:
+        new_dict_list.append(news.to_review_dict())
+
+    context = {
+        "current_page": current_page,
+        "total_page": total_page,
+        "news_list": new_dict_list
+
+    }
+    return render_template("admin/news_review.html", data=context)
