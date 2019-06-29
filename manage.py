d0bfd9324f7292b07317bc1aea43807cf6e408e7
li <945937725@qq.com>
2019-06-29 10:00:17 +0800

    集成创建管理员的方法到manage

--- manage.py
+++ manage.py
@@ -2,6 +2,13 @@ from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from info import db, create_app, models
 
+# manage.py是程序启动的入口，只关心启动的相关参数以及内容，不关心具体该
+# 如果创建app或者相关业务逻辑
+
+# 通过指定的配置名字创建对应配置的app
+# create_app 就类似于工厂方法
+from info.models import User
+
 app = create_app('development')
 # 设置项目运行参数
 manger = Manager(app)
@@ -9,6 +16,28 @@ manger = Manager(app)
 Migrate(app, db)
 # 将迁移命令添加到manger中
 manger.add_command('db', MigrateCommand)
+
+
+@manger.option('-n', '-name', dest='name')
+@manger.option('-p', '-password', dest="password")
+def createsuperuser(name, password):
+    if not all([name, password]):
+        print("参数不足")
+    user = User
+    user.nick_name = name
+    user.mobile = name
+    user.password = password
+    user.is_admin = True
+
+    try:
+        db.session.add(user)
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        print(e)
+
+    print("添加成功")
+
 
 if __name__ == '__main__':
     manger.run()
