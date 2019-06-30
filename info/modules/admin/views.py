16796f63cac8be067e8dd6b662db076d2df90596
li <945937725@qq.com>
2019-06-30 16:14:12 +0800

    审核详情的新闻数据加载

--- info/modules/admin/views.py
+++ info/modules/admin/views.py
@@ -1,7 +1,7 @@
 import time
 from datetime import datetime, timedelta
 
-from flask import current_app, g, jsonify
+from flask import current_app, g, jsonify, abort
 from flask import redirect
 from flask import render_template
 from flask import request
@@ -9,7 +9,7 @@ from flask import session
 from flask import url_for
 
 from info import constants
-from info.models import User, News
+from info.models import User, News, Category
 from info.modules.admin import admin_blu
 from info.utils.captcha.response_code import RET
 from info.utils.common import user_login_data
@@ -309,6 +309,48 @@ def news_edit():
 
 @admin_blu.route("/news_edit_detail")
 def news_edit_detail():
+    """新闻编辑详细信息展示"""
     # 查询点击的新闻的相关数据并传入到模板中
+    news_id = request.args.get("news_id")
 
-    return render_template("admin/news_edit_detail.html")
+    if not news_id:
+        abort(404)
+
+    try:
+        news_id = int(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return render_template("admin/news_edit_detail.html", errmsg="参数错误")
+
+    try:
+        news = News.query.get(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return render_template("admin/news_edit_detail.html", errmsg="数据查询错误")
+
+    if not news:
+        return render_template("admin/news_edit_detail.html", errmsg="新闻不存在")
+
+    # 查询分类数据
+    try:
+        categories = Category.query.all()
+    except Exception as e:
+        current_app.logger.error(e)
+        return render_template("admin/news_edit_detail.html", errmsg="数据查询错误")
+
+    category_dict_list = []
+    for category in categories:
+        # 取到分类的字典
+        cate_dict = category.to_dict()
+        # 判断当前遍历到的分类是否是当前新闻的分类，如果是，则添加is_selected为true
+        if category.id == news.category_id:
+            cate_dict["is_selected"] = True
+        category_dict_list.append(cate_dict)
+    category_dict_list.pop(0)
+
+    data = {
+        "news": news.to_dict(),
+        "categories": category_dict_list
+    }
+
+    return render_template("admin/news_edit_detail.html", data=data)
